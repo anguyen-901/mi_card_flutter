68a87e04133f46eff9915f3ff8538bd6c3e44e4f
Albert Nguyen <albertnguyen34@gmail.com>
2023-03-07 00:49:29 -0500

    Added Phone and Email card

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.green,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
@@ -36,6 +37,55 @@ class MyApp extends StatelessWidget {
                   fontSize: 20.0,
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "901-303-7437",
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: "Source_Sans_Pro",
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "albertnguyen34@gmail.com",
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: "Source_Sans_Pro",
+                      fontSize: 20.0,
+                    ),
+                  ),
                 ),
               ),
             ],
