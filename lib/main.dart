072bf27c56a483cf378ef310d604ce4d4d88a20c
Albert Nguyen <albertnguyen34@gmail.com>
2023-03-07 00:08:13 -0500

    Added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,26 @@ class MyApp extends StatelessWidget {
             children: [
               CircleAvatar(
                 radius: 50.0,
-                backgroundColor: Colors.blueGrey,
-                foregroundImage: AssetImage("images/IMG_3384.HEIC"),
+                backgroundImage: AssetImage("images/IMG_3384.HEIC"),
+              ),
+              Text(
+                "Albert Nguyen",
+                style: TextStyle(
+                  fontFamily: "Pacifico",
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FULL STACK DEVELOPER",
+                style: TextStyle(
+                  fontFamily: "Source_Sans_Pro",
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ],
           ),
